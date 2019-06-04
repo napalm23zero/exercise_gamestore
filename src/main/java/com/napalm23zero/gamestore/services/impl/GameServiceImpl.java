b5d8a1c8c45b2dee7d1fddc29e2688226862e7d8
Rodrigo Dantas <rodrigodantas.91@gmail.com>
2019-06-04 15:44:49 -0300

    Removing unised repository refereces

--- src/main/java/com/napalm23zero/gamestore/services/impl/GameServiceImpl.java
+++ src/main/java/com/napalm23zero/gamestore/services/impl/GameServiceImpl.java
@@ -1,17 +1,14 @@
 package com.napalm23zero.gamestore.services.impl;
 
 import com.napalm23zero.gamestore.models.Game;
-import com.napalm23zero.gamestore.repositories.GameRepository;
 import com.napalm23zero.gamestore.repositories.GenericRepository;
 import com.napalm23zero.gamestore.services.GameService;
+
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("gameService")
 public class GameServiceImpl extends GenericServiceImpl<Game, ObjectId> implements GameService {
-    @Autowired
-    private GameRepository repository;
 
     public GameServiceImpl(GenericRepository<Game, ObjectId> genericRepository) {
         super(genericRepository);
--- src/test/java/com/napalm23zero/gamestore/controllers/GameControllerTest.java
+++ src/test/java/com/napalm23zero/gamestore/controllers/GameControllerTest.java
@@ -2,8 +2,6 @@ package com.napalm23zero.gamestore.controllers;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class GameControllerTest {
 
     @Test
