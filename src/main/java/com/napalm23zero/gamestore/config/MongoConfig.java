e7da9ebcb20258a1ac8b36e451919d952875695d
Rodrigo <rodrigodantas.91@gmail.com>
2019-07-03 10:44:03 -0300

    NoWorking

--- src/main/java/com/napalm23zero/gamestore/config/MongoConfig.java
+++ src/main/java/com/napalm23zero/gamestore/config/MongoConfig.java
@@ -5,6 +5,7 @@ import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -16,27 +17,37 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = "com.napalm23zero.gamestore.repositories")
 public class MongoConfig extends AbstractMongoConfiguration {
 
-    @Value("${spring.datasource.mongo.username}")
-    private String username;
-    @Value("${spring.datasource.mongo.password}")
-    private String password;
-    @Value("${spring.datasource.mongo.database}")
-    private String database;
-    @Value("${spring.datasource.mongo.host}")
-    private String host;
-    @Value("${spring.datasource.mongo.port}")
-    private Integer port;
+    private final String username;
+    private final String password;
+    private final String database;
+    private final String host;
+    private final Integer port;
+
+    @Autowired
+    public MongoConfig(@Value("${spring.datasource.mongo.username}") String username,
+            @Value("${spring.datasource.mongo.password}") String password,
+            @Value("${spring.datasource.mongo.database}") String database,
+            @Value("${spring.datasource.mongo.host}") String host,
+            @Value("${spring.datasource.mongo.port}") Integer port) {
+        this.username = username;
+        this.password = password;
+        this.database = database;
+        this.host = host;
+        this.port = port;
+    }
 
     @Override
     public MongoClient mongoClient() {
-        MongoCredential mongoCredential = MongoCredential.createCredential(username, database, password.toCharArray());
-        ServerAddress serverAddress = new ServerAddress(host, port);
+        MongoCredential mongoCredential = MongoCredential.createCredential(this.username, this.database,
+                this.password.toCharArray());
+        ServerAddress serverAddress = new ServerAddress(this.host, this.port);
         MongoClientOptions mongoClientOptions = MongoClientOptions.builder().applicationName("gamestore").build();
         return new MongoClient(serverAddress, mongoCredential, mongoClientOptions);
     }
 
     @Override
     protected String getDatabaseName() {
-        return database;
+        return this.database;
     }
+
 }
